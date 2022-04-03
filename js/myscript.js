d94b87ce396971326f96f659816697c377490a20
rabiroshan shubhankar <rshubhankar11@gmail.com>
2022-04-03 16:33:42 +0530

    added new api call for specific api

--- js/myscript.js
+++ js/myscript.js
@@ -17,20 +17,45 @@ app.controller("MyCtrl", ($scope, $http) => {
     //calling Api to get all data 
 
     $http.get(urlAll).then(
-        (response) => {
-            //success 
-            console.log("API is called successfully ")
-            console.log(response.data)
+            (response) => {
+                //success 
+                console.log("API is called successfully ")
+                console.log(response.data)
 
-            $scope.all_data = response.data;
+                $scope.all_data = response.data;
 
-        },
-        (error) => {
-            //unsuccess
-            console.log(error)
+            },
+            (error) => {
+                //unsuccess
+                console.log(error)
 
+            }
+        )
+        //get country data
+
+    $scope.get_c_data = () => {
+        let country = $scope.c;
+        if (country == '') {
+            console.log("empty value for country")
+            $scope.c = undefined;
+            return
         }
-    )
+
+        $http.get(`${urlAll}/countries/${country}`).then((response) => {
+                //success
+                console.log("successfully called country api ")
+                console.log(response.data)
+                $scope.c_data = response.data
+
+            },
+            (error) => {
+                //error
+                console.log("Error calling country data api ")
+
+
+            });
+
+    }
 
 
 })
